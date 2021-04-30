c3c30e0d7cf8d6b5f2cc5005cdaf9bbd90becb4f
unknown <mubashar.gondal1@gmail.com>
2021-04-30 11:04:23 -0400

    first commit

--- debug_project_app/routes.py
+++ debug_project_app/routes.py
@@ -14,13 +14,13 @@ from flask_login import login_required,login_user, current_user,logout_user
 @app.route('/')
 def home():
     posts = Post.query.all
-    returnrender_template("homes.html", posts = posts)
+    return render_template("home.html", posts = posts, current_user = User)
 
 # Register Route
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = UserInfoForm()
-    if request.method = 'POST' and form.validate():
+    if request.method == 'POST' and form.validate():
         # Get Information
         username = form.username.data
         password = form.password.data
